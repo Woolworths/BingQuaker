ca7e19aa70a1b807a3c1683060fcd0ab5f77edd7
Jean Abed <jean.abed@hotmail.com>
2014-10-05 17:28:17 +1100

    clarification

--- BingQuaker/__init__.py
+++ BingQuaker/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "github.com/woolworths"
 __version__ = "1.0"
-__copyright__ = "Copyright (c) woolworths"
+__copyright__ = "Copyright (c) github.com/woolworths"
 __license__ = "The MIT Licence"
 
 from .core import Search
